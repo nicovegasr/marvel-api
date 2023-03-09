cb68c65a4506e825c5716a89374be2786285420a
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-03-09 13:48:52 +0000

    Añadiendo funcionalidad de resetear la base de datos todos los dias para tener actualizada la base de datos.

---
+++ src/middlewares/databaseReset.ts
@@ -0,0 +1,28 @@
+import { CharacterModel } from "../models/character"
+
+/**
+ * @description: Resets the database if the last Marvel character was added in a different day than today.
+ * @param res: the response object
+ * @param next: the next middleware function to call
+ * @returns: If the last Marvel character was added today, calls the next middleware function. Otherwise, drops the CharacterModel collection.
+*/
+export const resetDatabase = async (_: any, res: any, next: any) => {
+  const today = new Date();
+  const lastMarvelCharacter = await CharacterModel.findOne().sort({ _id: -1 });
+  const actualDate = today.toISOString().slice(0, 10);;
+  if (lastMarvelCharacter) {
+    const lastChangeDate = lastMarvelCharacter._id.getTimestamp().toISOString().slice(0, 10);
+    if (actualDate === lastChangeDate) {
+      next()
+    } else {
+      try { 
+        await CharacterModel.collection.drop()
+      } catch (err) {
+        return res.status(401).send({message: "Collection couldn't be deleted."})
+      }
+    }
+  }
+  else {
+    next()
+  }
+}
--- src/routes/characterRoute.ts
+++ src/routes/characterRoute.ts
@@ -3,11 +3,12 @@ import * as express from 'express';
 import { checkDatabase } from '../middlewares/databaseChecker';
 import { getById } from '../controllers/characterController';
 import { getByName } from '../controllers/characterController';
+import { resetDatabase } from '../middlewares/databaseReset';
 
 /**
  * Marvel Character`s endpoints
  */
 export const characterRouter = express.Router();
 
-characterRouter.get('/character/id/:id', checkDatabase, getById);
-characterRouter.get('/character/name/:name', checkDatabase, getByName);
+characterRouter.get('/character/id/:id', resetDatabase, checkDatabase, getById);
+characterRouter.get('/character/name/:name', resetDatabase, checkDatabase, getByName);
