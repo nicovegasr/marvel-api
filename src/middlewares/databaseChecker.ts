50c5e77cbebc0cafaa6f219c3f97c9500b6c87ef
Nicolas Vegas <alu0101321745@ull.edu.es>
2023-03-09 12:57:53 +0000

    Creacion de middlewares para consumo de api exterior.

---
+++ src/middlewares/databaseChecker.ts
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { CharacterModel } from "../models/character"
+import { CharacterRepositorie } from "../repositories/characterRepositorie";
+const md5 = require('md5');
+
+export const checkDatabase = async (_: any, res: any, next: any) => {
+  const count = await CharacterModel.count()
+  if (count > 0) {
+    next()
+  } else {
+    try {
+      const url = `http://gateway.marvel.com/v1/public/characters`
+      const publicApiKey = process.env.PUBLIC_API_KEY as string
+      const privateApiKey = process.env.PRIVATE_API_KEY as string
+      const ts = new Date().getTime();
+      const stringToHash = ts + privateApiKey + publicApiKey 
+      const hash = md5(stringToHash)
+      const params = {
+        ts: ts,
+        apikey: publicApiKey,
+        hash: hash
+      }
+      const response = await axios.get(url, {params})
+      const data = response.data.data.results
+      const repositorie = new CharacterRepositorie()
+      try {
+        repositorie.saveCharacters(data)
+      }
+      catch(error) {
+        res.status(204).send("Data couldn`t be stored.")
+      }
+      res.status(200).send("Data stored.");
+    } 
+    catch (err) {
+      res.status(401).send(err as string);
+    }
+  }
+}
